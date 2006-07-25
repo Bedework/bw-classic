31b6ed568fd4d54361489a22c19893b42e0eeb81
douglm <douglm@44982b2c-cc0a-0410-892b-d264454eb065>
2006-07-25 15:48:30 +0000

    caldavTest is now a separate project

--- calendar3/caldavTest/caldav-interop/configoraclecaldav.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-export T_USERNAME1=ioptest6@example.com
-export T_USERPASSWORD1=test1
-export T_USERMAIL1=douglm@rpi.edu
-export T_USERMAIL2=douglm2@rpi.edu
-export T_CALENDARHOSTNAME=http://caldavlinux.oracle.com
-export T_CALENDARRELATIVEURL=/ocaldav/home/ioptest6@example.com/calendars/events
-
