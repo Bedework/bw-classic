a70a114d56667d75bfcce0f0dd1f1cd0cc85027e
johnsa <johnsa@44982b2c-cc0a-0410-892b-d264454eb065>
2007-03-27 15:07:12 +0000

    admin client: clock widget now works in 12 hour mode

--- deployment/webadmin/webapp/resources/resources/bwClock.js
+++ deployment/webadmin/webapp/resources/resources/bwClock.js
@@ -33,9 +33,10 @@ function bwClockClose() {
   changeClass("clock","invisible");
 }
 
-function bwClockUpdateDateTimeForm(valType,val) {
+function bwClockUpdateDateTimeForm(valType,val,hour24) {
   // valType: "hour" or "minute"
   // val: hour or minute value as integer
+  // hour24: true (24hr clock) or false (12hr clock + am/pm) 
   if (bwClockRequestedType) {
     try {
       if (valType == 'minute') {
@@ -47,11 +48,28 @@ function bwClockUpdateDateTimeForm(valType,val) {
         bwClockMinute = val;
       } else {
         var fieldName = bwClockRequestedType + ".hour"
-        window.document.eventForm[fieldName].value = val;
-        if (val < 10) {
-          val = "0" + val; // pad the value for display
+        if (hour24) {
+          window.document.eventForm[fieldName].value = val;
+          if (val < 10) {
+            val = "0" + val; // pad the value for display
+          }
+          bwClockHour = val;
+        } else {
+          var hour12 = val;
+          if (hour12 > 12) {
+            hour12 -= 12;
+          } else if (hour12 == 12) {
+            hour12 = 0; // noon and midnight are both represented by '0' in 12hr mode
+          }
+          window.document.eventForm[fieldName].value = hour12;
+          if (val < 10) {
+            val = "0" + val; // pad the value for display
+          }
+          bwClockHour = val;
+          // now set the am/pm field
+          fieldName = bwClockRequestedType + ".ampm";
+          window.document.eventForm[fieldName].value = bwClockGetAmPm(bwClockHour);
         }
-        bwClockHour = val;
       }
       if (bwClockHour && bwClockMinute) {
         document.getElementById("bwClockTime").innerHTML = bwClockHour + ":" + bwClockMinute + " , " + bwClockConvertAmPm(bwClockHour) + ":" + bwClockMinute + " " + bwClockGetAmPm(bwClockHour);
@@ -82,8 +100,8 @@ function bwClockConvertAmPm(hour24) {
 function bwClockGetAmPm(hour24) {
   hour24 = parseInt(hour24,10);
   if (hour24 < 12) {
-    return 'a.m.';
+    return 'am';
   } else {
-    return 'p.m.';
+    return 'pm';
   }
 }
