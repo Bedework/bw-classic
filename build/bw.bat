00aee044daf4ba444de6afbd964c8561ef814277
johnsa <johnsa@44982b2c-cc0a-0410-892b-d264454eb065>
2008-07-20 02:53:03 +0000

    updates to build scripts

--- build/bw.bat
+++ build/bw.bat
@@ -1,122 +1,141 @@
-::
-:: This file is included by the quickstart script file bw.bat so that it can live
-:: within the svn repository.
-::
-set PRG=%0
+::  This file is included by the quickstart script file "..\..\bw.bat" so that 
+::  we may keep this script under version control in the svn repository.
 
-saveddir=`pwd`
+@ECHO off
+SETLOCAL
 
-export QUICKSTART_HOME=$saveddir
+ECHO.
+ECHO.
+ECHO   Bedework Calendar System
+ECHO   ------------------------
+ECHO.
+
+
+SET PRG=%0
+SET saveddir=%CD%
+SET QUICKSTART_HOME=%saveddir%
 
 :: Default some parameters
+SET BWCONFIGS=
+SET bwc=default
+SET BWCONFIG=
+SET offline=
+SET quickstart=
 
-set BWCONFIGS=
-set bwc=default
-set BWCONFIG=
-set offline=
-set quickstart=
+:: check for command-line arguments and branch on them
+IF "%1noargs" == "noargs" GOTO usage
+GOTO branch
 
-while [ "$1" != "" ]
-do
-  # Process the next arg
-  case $1       # Look at $1
-  in
-    -bwchome)         # Define location of configs
-      shift
-      set BWCONFIGS="$1"
-      shift
-      ;;
-    -quickstart)
-      set quickstart="yes"
-      shift
-      ;;
-    -usage | -help | -? | ?)
-      goto usage
-      ;;
-    -bwc)
-      set shift
-      set bwc="$1"
-      shift
-      ;;
-    -offline)
-      set offline="-Dorg.bedework.offline.build=yes"
-      shift
-      ;;
-    -*)
-      goto usage
-      ;;
-    *)
-::    Assume we've reached the target(s)
-      break
-      ;;
-  esac
-done
+:quickstart
+  ECHO     Preparing quickstart build ...
+  SET quickstart="yes"
+  SHIFT
+  IF "%1noarg" == "noarg" GOTO doneWithArgs
+  GOTO branch
+  
+:bwchome         
+  :: Define location of configs
+  SHIFT
+  SET BWCONFIGS=%1
+  SHIFT
+  IF "%1noarg" == "noarg" GOTO doneWithArgs
+  GOTO branch
 
-if "%quickstart%" != "" GOTO :checkBwConfig
-  if "%BWCONFIGS%" != "" GOTO doneQB
-    set BWCONFIGS=%HOME%\bwbuild
-GOTO :doneQB
+:bwc
+  SHIFT
+  SET bwc=%1
+  SHIFT
+  IF "%1noarg" == "noarg" GOTO doneWithArgs
+  GOTO branch
+  
+:offline
+  ECHO     Setting to offline mode; libraries will not be downloaded ...
+  SET offline="-Dorg.bedework.offline.build=yes"
+  SHIFT
+  IF "%1noarg" == "noarg" GOTO doneWithArgs
+  GOTO branch
+
+:doneWithArgs
+  
+IF NOT "%quickstart%empty" == "empty" GOTO checkBwConfig
+IF NOT "%BWCONFIGS%empty" == "empty" GOTO DoneQB
+SET BWCONFIGS=%HOME%\bwbuild
+GOTO doneQB
 
 :checkBwConfig
-  if "%BWCONFIGS" == "" GOTO QB
-    echo *******************************************************
-    echo Error: Cannot specify both -quickstart and -bwchome
-    echo *******************************************************
-    GOTO :EOF
+REM  IF "%BWCONFIGS%empty" == "empty" GOTO doneQB
+REM    ECHO *******************************************************
+REM    ECHO Error: Cannot specIFy both -quickstart and -bwchome
+REM    ECHO *******************************************************
+REM    GOTO:EOF
 
-  set BWCONFIGS=%QUICKSTART_HOME%\bedework\config\bwbuild
+  SET BWCONFIGS=%QUICKSTART_HOME%\bedework\config\bwbuild
 
 :doneQB
+  SET BEDEWORK_CONFIGS_HOME=%BWCONFIGS%
+  SET BEDEWORK_CONFIG=%BWCONFIGS%\%bwc%
 
-export BEDEWORK_CONFIGS_HOME=%BWCONFIGS%
-export BEDEWORK_CONFIG=%BWCONFIGS%\%bwc%
-
-if exist %BEDEWORK_CONFIG%/build.properties GOTO foundBuildProperties
-  echo *******************************************************
-  echo Error: Configuration %BEDEWORK_CONFIG% does not exist or is not a bedework configuration.
-  echo *******************************************************
-  GOTO :EOF
+  IF EXIST "%BEDEWORK_CONFIG%\build.properties" GOTO foundBuildProperties
+  ECHO *******************************************************
+  ECHO Error: Configuration %BEDEWORK_CONFIG% does not exist or is not a bedework configuration.
+  ECHO *******************************************************
+  GOTO:EOF
 :foundBuildProperties
 
-if not "%JAVA_HOME%"=="" goto javaOk
-  echo *******************************************************
-  echo Error: JAVA_HOME is not defined correctly for bedework.
-  echo *******************************************************
-  GOTO :EOF
+  IF NOT "%JAVA_HOME%empty"=="empty" GOTO javaOk
+  ECHO *******************************************************
+  ECHO Error: JAVA_HOME is not defined correctly for bedework.
+  ECHO *******************************************************
+  GOTO:EOF
 :javaOk
 
-:: Make available for ant
-set BWCONFIG=-Dorg.bedework.user.build.properties=%BEDEWORK_CONFIG%/build.properties"
+:runBedework
+  :: Make available for ant
+  SET BWCONFIG=-Dorg.bedework.user.build.properties=%BEDEWORK_CONFIG%\build.properties"
+  
+  ECHO.
+  ECHO     BWCONFIGS = %BWCONFIGS%
+  ECHO     BWCONFIG = %BWCONFIG%
+  ECHO. 
+  
+  SET ANT_HOME=%QUICKSTART_HOME%\apache-ant-1.7.0
+  
+  SET CLASSPATH="%ANT_HOME%\lib\ant-launcher.jar"
+  
+  "%JAVA_HOME%\bin\java.exe" -classpath %CLASSPATH% %offline% -Dant.home=%ANT_HOME% org.apache.tools.ant.launch.Launcher %BWCONFIG% %*%
+  
+  GOTO:EOF
 
-echo BWCONFIGS=%BWCONFIGS%
-echo BWCONFIG=%BWCONFIG%
-
-set ANT_HOME=`dirname "$PRG"`/apache-ant-1.7.0
-set ANT_HOME=`cd "$ANT_HOME" && pwd`
-
-set CLASSPATH=%ANT_HOME%/lib/ant-launcher.jar
-
-%JAVA_HOME%\bin\java.exe -classpath %CLASSPATH% %offline% -Dant.home=%ANT% org.apache.tools.ant.launch.Launcher %BWCONFIG% %*%
-
-GOTO :EOF
-
+  
+:: Iterate over the command line arguments;
+:: DOS Batch labels can't contain hyphens, so convert them
+:: (otherwise, we could just "GOTO %1")
+:branch
+IF "%1" == "-quickstart" GOTO quickstart
+IF "%1" == "-bwchome" GOTO bwchome
+IF "%1" == "-bwc" GOTO bwc
+IF "%1" == "-offline" GOTO offline
+  
 :usage
-  echo %PRG [-bwchome path | -quickstart ] [ -bwc configname ] [ -offline } [ target ]
-  echo
-  echo  -bwchome specify path to configurations
-  echo  -quickstart  Use the current quickstart configurations
-  echo  - bwc specify configuration name
-  echo  -offline  build without atempting to retrieve library jars
-  echo  target  ant target to execute
-  echo
-  echo Invokes ant to build or deploy the bedework system. Uses a configuration
-  echo directory which contains one directory per configuration.
-  echo
-  echo Within each configuration directory we expect a file called build.properties
-  echo which should point to the proerty and options file needed for the deploy process
-  echo
-  echo In general these files will be in the same directory as build.properties.
-  echo The environment variable BEDEWORK_CONFIG contains the path to the current
-  echo configuration directory and can be used to build a path to the other files.
-  echo
-
+  ECHO    Usage:
+  ECHO.
+  ECHO    %0 [ -quickstart OR -bwchome path ] [ -bwc configname ] [ -offline ] [ target ]
+  ECHO.
+  ECHO      -quickstart  Use the current quickstart configurations.  
+  ECHO      -bwchome     Specify path to configurations
+  ECHO      -bwc         Specify configuration name
+  ECHO      -offline     Build without atempting to retrieve library jars
+  ECHO      target       Ant target to execute
+  ECHO.
+  ECHO    Invokes ant to build or deploy the bedework system. Uses a configuration
+  ECHO    directory which contains one directory per configuration.
+  ECHO.
+  ECHO    Within each configuration directory we expect a file called 
+  ECHO    build.properties which should point to the property and options file 
+  ECHO    needed for the deploy process.
+  ECHO.
+  ECHO    In general these files will be in the same directory as build.properties.
+  ECHO    The environment variable BEDEWORK_CONFIG contains the path to the current
+  ECHO    configuration directory and can be used to build a path to the other files.
+  ECHO.
+  ECHO.
