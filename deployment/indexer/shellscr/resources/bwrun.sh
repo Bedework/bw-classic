8913088a1aa2d1315b2d466c4dd2e875bbb9e610
douglm <douglm@44982b2c-cc0a-0410-892b-d264454eb065>
2009-12-02 16:50:54 +0000

    Many updates to indexer crawler code to keep track of threads

--- deployment/indexer/shellscr/resources/bwrun.sh
+++ deployment/indexer/shellscr/resources/bwrun.sh
@@ -15,6 +15,8 @@ done
 TEMPDIR="./temp"
 mkdir $TEMPDIR
 
+mkdir logs
+
 RUNCMD="$JAVA_HOME/bin/java -Djava.io.tmpdir="$TEMPDIR" -cp $cp org.bedework.indexer.BwIndexApp"
 
 APPNAME=indexer
@@ -22,19 +24,25 @@ APPNAME=indexer
 
 case "$1" in
   reindex)
-    echo $RUNCMD -appname $APPNAME reindex $2 $3 $4 $5 $6 $7 $8 $9
-    $RUNCMD -appname $APPNAME reindex $2 $3 $4 $5 $6 $7 $8 $9
+    echo $RUNCMD -appname $APPNAME -crawl -listen $2 $3 $4 $5 $6 $7 $8 $9
+    $RUNCMD -appname $APPNAME -crawl -listen $2 $3 $4 $5 $6 $7 $8 $9
+    ;;
+  reindex-nostart)
+    echo $RUNCMD -appname $APPNAME -crawl $2 $3 $4 $5 $6 $7 $8 $9
+    $RUNCMD -appname $APPNAME -crawl $2 $3 $4 $5 $6 $7 $8 $9
     ;;
   start)
-    echo $RUNCMD -appname $APPNAME start $2 $3 $4 $5 $6 $7 $8 $9
-    $RUNCMD -appname $APPNAME start $2 $3 $4 $5 $6 $7 $8 $9
+    echo $RUNCMD -appname $APPNAME -listen $2 $3 $4 $5 $6 $7 $8 $9
+    $RUNCMD -appname $APPNAME -listen $2 $3 $4 $5 $6 $7 $8 $9
     ;;
   *)
     echo $" "
     echo $"Usage: "
-    echo $"  $0 reindex [(-ndebug | -debug)] "
+    echo $"  $0 reindex "
     echo $"     Reindex the system then process queue events"
-    echo $"  $0 start [(-ndebug | -debug)] "
+    echo $"  $0 reindex-nostart "
+    echo $"     Reindex the system. Do not process queue events"
+    echo $"  $0 start"
     echo $"     Process queue events"
     echo $" "
 esac
