88e6603b27346cddc7d00e4d3fbe771e0fb6400f
johnsa <johnsa@44982b2c-cc0a-0410-892b-d264454eb065>
2010-02-12 20:25:25 +0000

    port startjboss script enhancements to windows (ability to set JVM settings) and update usage text

--- build/quickstart/windows/startjboss.bat
+++ build/quickstart/windows/startjboss.bat
@@ -6,6 +6,34 @@ SETLOCAL
 :: (via a source)
 
 SET BASE_DIR=%CD%
+SET PRG=%0
+
+:: Set defaults
+SET heap=1G
+SET newsize=330M
+SET permsize=256M
+
+GOTO branch
+
+:heap
+  SHIFT
+  SET heap=%1
+  SHIFT
+  GOTO branch
+
+:newsize
+  SHIFT
+  SET newsize=%1
+  SHIFT
+  GOTO branch
+
+:permsize
+  SHIFT
+  SET permsize=%1
+  SHIFT
+  GOTO branch
+
+:doneWithArgs
 
 SET JBOSS_VERSION=jboss-5.1.0.GA
 SET JBOSS_CONFIG=default
@@ -33,6 +61,10 @@ SET LOG_THRESHOLD=-Djboss.server.log.threshold=DEBUG
 SET BW_DATA_DIR=%JBOSS_DATA_DIR%\bedework
 SET BW_DATA_DIR_DEF=-Dorg.bedework.data.dir=%BW_DATA_DIR%\
 
+SET JAVA_OPTS=%JAVA_OPTS% -Xms%heap% -Xmx%heap%
+SET JAVA_OPTS=%JAVA_OPTS% -XX:NewSize=%newsize% -XX:MaxNewSize=%newsize%
+SET JAVA_OPTS=%JAVA_OPTS% -XX:PermSize=%permsize% -XX:MaxPermSize=%permsize%
+
 SET RUN_CMD=.\%JBOSS_VERSION%\bin\run.bat -c %JBOSS_CONFIG% %JBOSS_BIND% %JBOSS_PORTS% %LOG_THRESHOLD% %ACTIVEMQ_DIRPREFIX% %BW_DATA_DIR_DEF%
 
 ECHO.
@@ -42,4 +74,40 @@ ECHO.
 ECHO.
 
 %RUN_CMD%
+GOTO:EOF
+
+
+:: Iterate over the command line arguments;
+:: DOS Batch labels can't contain hyphens, so convert them
+:: (otherwise, we could just "GOTO %1")
+:branch
+IF "%1" == "-usage" GOTO usage
+IF "%1" == "-heap" GOTO heap
+IF "%1" == "-newsize" GOTO newsize
+IF "%1" == "-permsize" GOTO permsize
+GOTO doneWithArgs
  
+:usage
+ECHO.
+ECHO.
+ECHO.
+ECHO startjboss.bat [-heap size] [-newsize size] [-permsize size]
+ECHO.
+ECHO Where:
+ECHO.
+ECHO -heap sets the heap size and should be n for bytes
+ECHO                                        nK for kilo-bytes (e.g. 256K)
+ECHO                                        nM for mega-bytes (e.g. 256M)
+ECHO                                        nG for giga-bytes (e.g. 1G)
+ECHO.
+ECHO -newsize sets the new generation size and has the same form as -heap
+ECHO  the value should be around one third of the heap
+ECHO.
+ECHO -permsize sets the permgen size and has the same form as -heap
+ECHO   The value should probably not be less than 256M
+ECHO.
+ECHO.
+ECHO. Default settings:
+ECHO. heap    = %heap%
+ECHO. newsize = %newsize%
+ECHO. permgen = %permsize%
