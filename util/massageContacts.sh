41d7e6df8ffb410e9fa0274c886c8c0f3acbf6f8
bleibson <bleibson@44982b2c-cc0a-0410-892b-d264454eb065>
2010-06-30 16:41:58 +0000

    script for massaging contact data

---
+++ util/massageContacts.sh
@@ -0,0 +1,70 @@
+#! /bin/bash -f
+
+inputFile=MimisContacts.txt
+tmpFile=/tmp/MimisContactsMassaged$$.txt
+# the resulting file is pumped out via stdout
+
+sed '/ADR;TYPE=WORK:;;;;;;$/d' $inputFile | sed '/ADR;TYPE=HOME:;;;;;;$/d' | sed '/ADR;TYPE=X-ORACLE-OTHER:;;;;;;$/d' | sed 's/VERSION:3.0$/VERSION:4.0/' > $tmpFile
+
+
+while read line; do 
+   
+  case "$line" in
+     ^$)
+       # toss empty lines
+       ;;
+     BEGIN:VCARD)
+       # save the Begin line
+       beginLine=$line
+       ;;
+     N:*)
+       # save N: lines for examination
+       nLine=$line
+       cardBuffer=${cardBuffer}${line}%%
+       ;;
+     ORG:*)
+       # save ORG: lines for examination
+       orgLine=$line
+       cardBuffer=${cardBuffer}${line}%%
+       ;;
+     FN:*)
+       # save FN: lines for examination
+       fnLine=$line
+       cardBuffer=${cardBuffer}${line}%%
+       ;;
+     END:VCARD)
+       #process what we have
+       # echo out the begin line
+       echo $beginLine
+       if [ "$fnLine" == "" ]; then
+          # mock up an FN and spit it out
+          if [ "$nLine" != "N:;;;;" ]; then
+            newFnLine=`echo $nLine | sed 's/N://'`
+            lastName=`echo "$newFnLine" | cut -d\; -f1`
+            firstName=`echo "$newFnLine" | cut -d\; -f2`
+            echo "FN:$firstName $lastName" | sed 's/FN: /FN:/'
+          else
+            newFnLine=`echo $orgLine | sed 's/ORG: /FN:/' |  sed 's/FN: /FN:/'`
+          fi
+          
+       fi
+       # echo out the rest of the lines
+       echo -n $cardBuffer | sed 's/%%/\n/g'
+       #echo out the end line
+       echo $line
+       fnLine=""
+       cardBuffer=""
+       nLine=""
+       orgLine=""
+       ;;
+     *)
+       cardBuffer=${cardBuffer}${line}%%
+       ;;
+esac      
+
+done < $tmpFile
+
+
+
+
+
