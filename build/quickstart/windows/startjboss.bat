ac31a438dc4b4d501c7576f2c0996a48fa4b12bb
douglm <douglm@44982b2c-cc0a-0410-892b-d264454eb065>
2010-08-20 15:01:03 +0000

    Use a system variable to set activemq bedework broker uri - changes all configs Change startjboss scripts to allow setting of activemquri at startup. Also allow setting of portoffset

--- build/quickstart/windows/startjboss.bat
+++ build/quickstart/windows/startjboss.bat
@@ -13,6 +13,10 @@ SET heap=600M
 SET newsize=200M
 SET permsize=256M
 
+SET activemquri=vm://localhost
+
+SET portoffset=0
+
 GOTO branch
 
 :heap
@@ -33,6 +37,18 @@ GOTO branch
   SHIFT
   GOTO branch
 
+:portoffset
+  SHIFT
+  SET portoffset=%1
+  SHIFT
+  GOTO branch
+
+:activemquri
+  SHIFT
+  SET activemquri=%1
+  SHIFT
+  GOTO branch
+
 :doneWithArgs
 
 SET JBOSS_VERSION=jboss-5.1.0.GA
@@ -49,12 +65,13 @@ SET JBOSS_BIND=-b 0.0.0.0
 ::
 
 :: standard ports
-SET JBOSS_PORTS=-Dorg.bedework.system.ports.offset=0
+SET JBOSS_PORTS=-Dorg.bedework.system.ports.offset=%portoffset%
 
 :: standard ports + defined value
 ::JBOSS_PORTS=-Dorg.bedework.system.ports.offset=505 -Djboss.service.binding.set=ports-syspar
 
 SET ACTIVEMQ_DIRPREFIX=-Dorg.apache.activemq.default.directory.prefix=%JBOSS_DATA_DIR%\
+SET ACTIVEMQ_URI=-Dorg.bedework.activemq.uri=%activemquri%
 
 SET LOG_THRESHOLD=-Djboss.server.log.threshold=DEBUG
 
@@ -65,7 +82,7 @@ SET JAVA_OPTS=%JAVA_OPTS% -Xms%heap% -Xmx%heap%
 SET JAVA_OPTS=%JAVA_OPTS% -XX:NewSize=%newsize% -XX:MaxNewSize=%newsize%
 SET JAVA_OPTS=%JAVA_OPTS% -XX:PermSize=%permsize% -XX:MaxPermSize=%permsize%
 
-SET RUN_CMD=.\%JBOSS_VERSION%\bin\run.bat -c %JBOSS_CONFIG% %JBOSS_BIND% %JBOSS_PORTS% %LOG_THRESHOLD% %ACTIVEMQ_DIRPREFIX% %BW_DATA_DIR_DEF%
+SET RUN_CMD=.\%JBOSS_VERSION%\bin\run.bat -c %JBOSS_CONFIG% %JBOSS_BIND% %JBOSS_PORTS% %LOG_THRESHOLD% %ACTIVEMQ_DIRPREFIX% %ACTIVEMQ_URI% %BW_DATA_DIR_DEF%
 
 ECHO.
 ECHO Starting Bedework JBoss:
@@ -85,6 +102,8 @@ IF "%1" == "-usage" GOTO usage
 IF "%1" == "-heap" GOTO heap
 IF "%1" == "-newsize" GOTO newsize
 IF "%1" == "-permsize" GOTO permsize
+IF "%1" == "-portoffset" GOTO portoffset
+IF "%1" == "-activemquri" GOTO activemquri
 GOTO doneWithArgs
  
 :usage
@@ -92,6 +111,7 @@ ECHO.
 ECHO.
 ECHO.
 ECHO startjboss.bat [-heap size] [-newsize size] [-permsize size]
+ECHO                [-portoffset n] [-activemquri uri]
 ECHO.
 ECHO Where:
 ECHO.
@@ -99,15 +119,22 @@ ECHO -heap sets the heap size and should be n for bytes
 ECHO                                        nK for kilo-bytes (e.g. 2560000K)
 ECHO                                        nM for mega-bytes (e.g. 256oM)
 ECHO                                        nG for giga-bytes (e.g. 1G)
+ECHO. Default: %heap%
 ECHO.
 ECHO -newsize sets the new generation size and has the same form as -heap
 ECHO  the value should be around one third of the heap
+ECHO. Default: %newsize%
 ECHO.
 ECHO -permsize sets the permgen size and has the same form as -heap
 ECHO   The value should probably not be less than 256M
+ECHO. Default: %permsize%
 ECHO.
+ECHO. -activemquri sets the uri used by the activemq broker for bedework
+ECHO.  Some possibilities: vm://localhost tcp://localhost:61616
+ECHO.  Default: %activemquriDefault%
 ECHO.
-ECHO. Default settings:
-ECHO. heap    = %heap%
-ECHO. newsize = %newsize%
-ECHO. permgen = %permsize%
+ECHO. -portoffset sets the offset for the standard jboss ports allowing
+ECHO.  multiple instances to be run on the same machine. The activemquri
+ECHO.  value may need to be set explicitly if it uses a port.
+ECHO.  Default: %portoffset%
+ECHO.
