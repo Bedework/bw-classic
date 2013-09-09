73ce0d33ceca69c96928305cd40a25cfae8131c9
douglm <douglm@rpi.edu>
2013-09-08 23:45:53 -0400

    Fix eventreg for hibernate interface issue.
    Also eventreg fixes for new bw-util.
    
    Add bw-util iml files

--- util/gitinstall.sh
+++ util/gitinstall.sh
@@ -7,7 +7,7 @@ cd $repo
 install() {
   git clone git@github.com:Bedework/$1.git
   cd $1/
-  mvn -q clean package 
+  mvn -q clean package
   mvn -q install
   cd ..
 }
@@ -18,8 +18,9 @@ install() {
 #install "bw-synch"
 #install "bw-timezone-server"
 #install "bw-webdav"
-install "bw-caldav"
+#install "bw-caldav"
 #install "bw-carddav"
 #install "bw-self-registration"
 #install "bw-calendar-engine"
 #install "bw-calendar-client"
+install "bw-event-registration"
